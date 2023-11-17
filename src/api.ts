cb02594619786b3144fd4ac44ad279e0184aeb29
eeee0717 <70054568+eeee0717@users.noreply.github.com>
2023-11-17 10:04:38 +0800

    fix url bug

--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,23 @@
 import { runAppleScript } from "run-applescript";
 
-export async function openUrl(urls: string[]) {
-
+export async function openUrl(urls: string) {
+  console.log('openUrl',urls);
   // workspace.urls = 'https://baidu.com,https://google.com';
-  urls = urls.split(',');
+  const split_urls = urls.split(',');
+  console.log('url_split',split_urls);
+
   // 修除空格
-  urls.forEach((url, index) => {
-    urls[index] = url.trim();
+  split_urls.forEach((url, index) => {
+    split_urls[index] = url.trim();
   })
-  console.log(urls);
-  for (const url of urls) {
+  for (const url of split_urls) {
     await runAppleScript(`open location "${url}"`);
   }
 }
 
 export async function openFile(files: string[]) {
   //用默认程序打开文件夹
-  console.log(files.length);
+  console.log('openFile',files);
   for (const file of files) {
     console.log(file);
     await runAppleScript(`
